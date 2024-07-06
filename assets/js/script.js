91b61d1732ec432a32506613076d0b83d43759ea
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-07-06 11:16:56 -0300

    barraMenu-finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-let li = document.querySelectorAll('.menu li').forEach(item => {
+document.querySelectorAll('.menu .li').forEach(item => {
     item.addEventListener('click', clickEvent)
 })
 
 function clickEvent(event) {
-    document.querySelector('.menu li.active').classList.remove('active')
+    document.querySelector('.menu .li.active').classList.remove('active')
     event.target.classList.add('active')
 }
