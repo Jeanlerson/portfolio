ec0ad51f8bc0fab1cd2b904c23398868ccf847f9
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-07-11 15:01:56 -0300

    skills-finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,10 @@ document.querySelectorAll('.menu .li').forEach(item => {
 })
 window.addEventListener('scroll', scrollHeader)
 document.querySelectorAll('.skills-img img').forEach(item => {
-    item.addEventListener('click', renderInforSillkFrame1)
+    item.addEventListener('mouseover', renderInforSillkFrame)
+    item.addEventListener('mouseout', returnToNormal)
 })
-document.querySelectorAll('.skills-img img').forEach(item => {
-    item.addEventListener('click', renderInforSillkFrame2)
-})
+
 // FUNCTIONS
 function clickEvent(event) {
     document.querySelector('.menu .li.active').classList.remove('active')
@@ -18,25 +17,23 @@ function scrollHeader() {
     let header = document.querySelector('header')
     header.classList.toggle('scrollMenu', window.scrollY > 0)
 }
-function renderInforSillkFrame1 (e) {
-    let infor1 = document.querySelector('#infor1')
+function renderInforSillkFrame (e) {
     let skill = e.target.getAttribute('data-descSkill')
     let renderSkill = document.querySelector(`p[data-descSkill=${skill}]`)
+    let valueSkill = renderSkill.textContent
 
     if ((skill === 'skill1') || (skill === 'skill2') || (skill === 'skill3') || (skill === 'skill4')) {
-        let valueSkill = renderSkill.textContent
+        let infor1 = document.querySelector('#infor1')
         infor1.textContent = valueSkill
-    }
-
-}
-function renderInforSillkFrame2 (e) {
-    let infor2 = document.querySelector('#infor2')
-    let skill = e.target.getAttribute('data-descSkill')
-    let renderSkill = document.querySelector(`p[data-descSkill=${skill}]`)
-
-    if ((skill === 'skill5') || (skill === 'skill6') || (skill === 'skill7') || (skill === 'skill8')) {
-        let valueSkill = renderSkill.textContent
+    } else {
+        let infor2 = document.querySelector('#infor2')
         infor2.textContent = valueSkill
     }
+}
+function returnToNormal () {
+    let infor1 = document.querySelector('#infor1')
+    let infor2 = document.querySelector('#infor2')
 
+    infor1.textContent = "Hover over the skill"
+    infor2.textContent = "Hover over the skill"
 }
