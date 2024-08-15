31ba289907076f044e15d8112beade1505a1db2e
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-08-15 17:18:34 -0300

    portfolio-finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,14 @@ function colorMenu () {
         }
     })
 }
+
+// MOBILE
+window.onload = function(){
+    document.querySelector(".menuMobile").addEventListener("click", function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex') {
+            document.querySelector(".menu nav ul").style.display = 'none';
+        } else{
+            document.querySelector(".menu nav ul").style.display = 'flex';
+        }
+    })
+};
